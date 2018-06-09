e283686b653f992a0f6d0bec71ec9fbbc401fbd7
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-06-08 22:03:39 -0400

    Klean kanvas.

--- Canvas.js
+++ Canvas.js
@@ -1,7 +1,5 @@
 class Canvas {
-	
 	constructor(id, width, height) {
-		
 		this.canvas = document.getElementById(id);
 		this.canvas.width = this.width = width;
 		this.canvas.height = this.height = height;
@@ -14,50 +12,5 @@ class Canvas {
 		this.canvas.style.left = "0px";
 		this.canvas.style.bottom = "0px";
 		this.canvas.style.right = "0px";
-		// this.canvas.style.backgroundColor = "white"; // why bother?
-		// this.canvas.style.cursor = "none"; // Was only temporary!
-		
 	}
-	
-	/* rect(x, y, width, height, colour) { // not this time!
-		
-		this.draw.fillStyle = colour;
-		this.draw.fillRect(x, y, width, height);
-		
-	}
-	
-	circle(x, y, radius, colour) {
-		
-		this.draw.beginPath();
-		this.draw.fillStyle = colour;		
-		this.draw.arc(x, y, radius, 0, 2 * Math.PI);
-		this.draw.fill();
-		this.draw.closePath();
-		
-	}
-	
-	text(text, x, y, colour, size, font, extras) { // for now, useless
-		
-		if(extras != undefined) {
-			
-			this.draw.font = extras + " " + size + "px " + font;
-			
-		} else {
-			
-			this.draw.font = size + "px " + font;
-			
-		}
-		
-		this.draw.fillStyle = colour;
-		this.draw.textAlign = "center";
-		this.draw.fillText(text, x, y + (size / 2));
-		
-	}
-	
-	clear_screen() {
-		
-		this.draw.clearRect(0, 0, this.width, this.height);
-	
-	} */
-		
 }
